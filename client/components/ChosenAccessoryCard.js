d5fdf2d73fe50583396ab0caadde7c6217742546
Shanntal Morel <51673250+Shanntal@users.noreply.github.com>
2021-08-13 11:05:31 -0400

    functionality transferred to components

--- client/components/ChosenAccessoryCard.js
+++ client/components/ChosenAccessoryCard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import StarRatings from "react-star-ratings";
 
 //useState-> function, pass in arg as a default value. Will use default value to create state for this compondent and will return an array with 2 times. 1st- value, 2nd setState function exclusively for that value
 
@@ -9,8 +10,17 @@ const ChosenAccessoryCard = ({ chosenAccessory }) => {
         <div key= { chosenAccessory.id }>
             <div className= 'singleItem'>
                 <div className= 'chosenAccessory' key={chosenAccessory.id}>
+                    <span>
+                        <StarRatings
+                            rating={chosenAccessory.rating * 1}
+                            starRatedColor="gold"
+                            numberOfStars={5}
+                            name="rating"
+                            starDimension="15px"
+                            starSpacing="0px"
+                        />
+                    </span>
                     <img src={chosenAccessory.imageUrl} />
-                    <p>Rating: { chosenAccessory.rating }</p>
                     <h1>{ chosenAccessory.name }</h1>
                     <p>{ chosenAccessory.description }</p>
                     <p>{ chosenAccessory.weight }oz</p>
@@ -23,12 +33,12 @@ const ChosenAccessoryCard = ({ chosenAccessory }) => {
             </div>
             
             <ul>
-                <button onClick={() => setCount(count - 1)}>-</button>
+                <button onClick={() => count > 0 && setCount(count - 1)}>-</button>
                 <li>{count}</li>
                 <button onClick={() => setCount(count + 1)}>+</button>
             </ul>
             <p>${ chosenAccessory.price }</p>
-            <button>ADD TO CART</button>
+            <button className={count > 0 ? "cta" : "ctadisabled"}>ADD TO CART</button>
         </div>
     )
 }
