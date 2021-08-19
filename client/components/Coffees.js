3e1fd03f2298d07f19fc0cd68da450f555e75be5
evkim1019 <kimgem89@gmail.com>
2021-08-19 11:39:10 -0400

    more css added, admin product failed to update

--- client/components/Coffees.js
+++ client/components/Coffees.js
@@ -20,14 +20,14 @@ export class Coffees extends React.Component {
   async componentDidMount() {
     await this.props.getProducts();
     this.setState({ coffees: this.props.coffees });
-    this.setState({loading: false});
+    this.setState({ loading: false });
   }
 
   handleSort = (ev) => {
     const sortBy = ev.target.value;
     const sortProducts = [...this.state.coffees];
-    sortProducts.sort((a,b) => {
-      switch (sortBy){
+    sortProducts.sort((a, b) => {
+      switch (sortBy) {
         case 'a-z':
           return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
         case 'z-a':
@@ -39,7 +39,7 @@ export class Coffees extends React.Component {
         default: // includes 'featured'
           return a.isFeatured && b.isFeatured ? 0 : a.isFeatured ? -1 : 1;
       }
-      })
+    })
     this.setState({ coffees: sortProducts })
   }
 
@@ -49,14 +49,14 @@ export class Coffees extends React.Component {
     let newFeaturedSelected = this.state.featuredSelected;
     let newSaleSelected = this.state.saleSelected;
 
-    switch(ev.target.name){
+    switch (ev.target.name) {
       case 'region':
         newRegion = ev.target.value;
-        this.setState({filteredRegion: newRegion});
+        this.setState({ filteredRegion: newRegion });
         break;
       case 'category':
         newCategory = ev.target.value;
-        this.setState({filteredCategory: newCategory});
+        this.setState({ filteredCategory: newCategory });
         break;
       case 'featured':
         newFeaturedSelected = true;
@@ -66,19 +66,19 @@ export class Coffees extends React.Component {
         newSaleSelected = true;
         this.setState({ saleSelected: true });
         break;
-      }
+    }
 
     let newList = [...this.state.coffees];
-    if (newRegion){
+    if (newRegion) {
       newList = newList.filter(c => c.country.region.toLowerCase() === newRegion);
     }
-    if (newCategory){
+    if (newCategory) {
       newList = newList.filter(c => c.category.toLowerCase() === newCategory);
     }
-    if (newFeaturedSelected){
+    if (newFeaturedSelected) {
       newList = newList.filter(c => c.isFeatured);
     }
-    if (newSaleSelected){
+    if (newSaleSelected) {
       newList = newList.filter(c => c.onSale);
     }
 
@@ -90,107 +90,107 @@ export class Coffees extends React.Component {
   }
 
   render() {
-    if (this.state.loading){
-      return <h2 style={{marginTop: 150, marginBottom: 200, textAlign: 'center'}}>Please wait while we load your favorite beverages!</h2>
+    if (this.state.loading) {
+      return <h2 style={{ marginTop: 150, marginBottom: 200, textAlign: 'center' }}>Please wait while we load your favorite beverages!</h2>
     }
 
     const coffees = this.state.coffees;
 
     return (
       <div id='content-wrapper'>
-      <Toaster /> 
+        <Toaster />
 
-      <div className="container filterList">
-        <div className="filter">
-          <div className="sortBy">
-            <span>Sort by</span>
-            <select name="sort" onChange={ this.handleSort }>
-              <option value="a-z">A - Z</option>
-              <option value="z-a">Z - A</option>
-              <option value="lowtohigh">Price low to high</option>
-              <option value="hightolow">Price high to low</option>
-            </select>
-          </div>
-          <br /><br />
-          <span>Filter <button onClick={this.resetFilter}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(reset filter)</button></span>
-          <hr />
-          <span><strong>Special</strong></span>
-          <div className="Featured">
-            <div>
-              <input type="radio" id="featured" name="featured" value="featured" onClick={ this.handleClick }></input>
-              <label htmlFor="featured">Featured</label>
+        <div className="container filterList">
+          <div className="filter">
+            <div className="sortBy">
+              <span>Sort by</span>
+              <select name="sort" onChange={this.handleSort}>
+                <option value="a-z">A - Z</option>
+                <option value="z-a">Z - A</option>
+                <option value="lowtohigh">Price low to high</option>
+                <option value="hightolow">Price high to low</option>
+              </select>
             </div>
-          </div>
-          <span><strong></strong></span>
-          <div className="Sale">
-            <div>
-              <input type="radio" id="sale" name="sale" value="sale" onClick={ this.handleClick }></input>
-              <label htmlFor="sale">On sale</label>
-            </div>
-          </div>
-          <span><strong>Origin</strong></span>
-          <div className="Region">
-            <div>
-              <input type="radio" id="africa" name="region" value="africa" onClick={ this.handleClick }></input>
-              <label htmlFor="africa">Africa</label>
-            </div>
-            <div>
-              <input type="radio" id="asia" name="region" value="asia" onClick={ this.handleClick }></input>
-              <label htmlFor="asia">Asia</label>
-            </div>
-            <div>
-              <input type="radio" id="centralAmerica" name="region" value="central america" onClick={ this.handleClick }></input>
-              <label htmlFor="centralAmerica">Central America</label>
-            </div>
-            <div>
-              <input type="radio" id="southAmerica" name="region" value="south america" onClick={ this.handleClick }></input>
-              <label htmlFor="southAmerica">South America</label>
-            </div>
-          </div>
-          <div className="Roast">
-            <span><strong>Roast</strong></span>
-            <div>
-              <input type="radio" id="dark" name="category" value="roast-dark" onClick={ this.handleClick }></input>
-              <label htmlFor="dark">Dark</label>
-            </div>
-            <div>
-              <input type="radio" id="medium" name="category" value="roast-medium" onClick={ this.handleClick }></input>
-              <label htmlFor="medium">Medium</label>
-            </div>
-            <div>
-              <input type="radio" id="light" name="category" value="roast-light" onClick={ this.handleClick }></input>
-              <label htmlFor="light">Light</label>
-            </div>
-          </div>
-          <div className="Type">
-            <span><strong>Type</strong></span>
-            <div>
-              <input type="radio" id="decaffeinated" name="category" value="decaf" onClick={ this.handleClick }></input>
-              <label htmlFor="decaffeinated">Decaffeinated</label>
-            </div>
-            <div>
-              <input type="radio" id="flavored" name="category" value="flavored" onClick={ this.handleClick }></input>
-              <label htmlFor="flavored">Flavored</label>
-            </div>
-            <div>
-              <input type="radio" id="organic" name="category" value="organic" onClick={ this.handleClick }></input>
-              <label htmlFor="organic">Organic</label>
-            </div>
-          </div>
-        </div>
-        {
-          coffees.length === 0 ? 
-            <h3>Sorry, no coffees match the filter criteria. Try a broader selection please.</h3>
-          :
-            <div className="itemList col3">
+            <br /><br />
+            <span>Filter <button className="hyperlink" onClick={this.resetFilter}>Reset Filter</button></span>
+            <hr />
+            <span><strong>Special</strong></span>
+            <div className="Featured">
               <div>
-                {coffees.map((coffee) => (
-                  <AllCoffeesCard coffee={coffee} key={coffee.id} />
-                ))}
+                <input type="radio" id="featured" name="featured" value="featured" onClick={this.handleClick}></input>
+                <label htmlFor="featured">Featured</label>
               </div>
             </div>
-        }
-      </div>
+            <span><strong></strong></span>
+            <div className="Sale">
+              <div>
+                <input type="radio" id="sale" name="sale" value="sale" onClick={this.handleClick}></input>
+                <label htmlFor="sale">On sale</label>
+              </div>
+            </div>
+            <span><strong>Origin</strong></span>
+            <div className="Region">
+              <div>
+                <input type="radio" id="africa" name="region" value="africa" onClick={this.handleClick}></input>
+                <label htmlFor="africa">Africa</label>
+              </div>
+              <div>
+                <input type="radio" id="asia" name="region" value="asia" onClick={this.handleClick}></input>
+                <label htmlFor="asia">Asia</label>
+              </div>
+              <div>
+                <input type="radio" id="centralAmerica" name="region" value="central america" onClick={this.handleClick}></input>
+                <label htmlFor="centralAmerica">Central America</label>
+              </div>
+              <div>
+                <input type="radio" id="southAmerica" name="region" value="south america" onClick={this.handleClick}></input>
+                <label htmlFor="southAmerica">South America</label>
+              </div>
+            </div>
+            <div className="Roast">
+              <span><strong>Roast</strong></span>
+              <div>
+                <input type="radio" id="dark" name="category" value="roast-dark" onClick={this.handleClick}></input>
+                <label htmlFor="dark">Dark</label>
+              </div>
+              <div>
+                <input type="radio" id="medium" name="category" value="roast-medium" onClick={this.handleClick}></input>
+                <label htmlFor="medium">Medium</label>
+              </div>
+              <div>
+                <input type="radio" id="light" name="category" value="roast-light" onClick={this.handleClick}></input>
+                <label htmlFor="light">Light</label>
+              </div>
+            </div>
+            <div className="Type">
+              <span><strong>Type</strong></span>
+              <div>
+                <input type="radio" id="decaffeinated" name="category" value="decaf" onClick={this.handleClick}></input>
+                <label htmlFor="decaffeinated">Decaffeinated</label>
+              </div>
+              <div>
+                <input type="radio" id="flavored" name="category" value="flavored" onClick={this.handleClick}></input>
+                <label htmlFor="flavored">Flavored</label>
+              </div>
+              <div>
+                <input type="radio" id="organic" name="category" value="organic" onClick={this.handleClick}></input>
+                <label htmlFor="organic">Organic</label>
+              </div>
+            </div>
+          </div>
+          {
+            coffees.length === 0 ?
+              <h3>Sorry, no coffees match the filter criteria. Try a broader selection please.</h3>
+              :
+              <div className="itemList col3">
+                <div>
+                  {coffees.map((coffee) => (
+                    <AllCoffeesCard coffee={coffee} key={coffee.id} />
+                  ))}
+                </div>
+              </div>
+          }
+        </div>
       </div>
     );
   }
@@ -198,7 +198,7 @@ export class Coffees extends React.Component {
 
 const mapState = (state) => {
   return {
-    coffees: state.products.filter((product) => product.type === "coffee").sort((a,b) => a.name.localeCompare(b.name)),
+    coffees: state.products.filter((product) => product.type === "coffee").sort((a, b) => a.name.localeCompare(b.name)),
   };
 };
 
